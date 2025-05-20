7fc60701128f031e75237a8a79f1f4e46623c867
mohyware <mohieelden50@gmail.com>
2025-05-20 10:33:09 +0300

    add tests for exomol and nist

--- backend/__tests__/src/test_calculateSpectrum.py
+++ backend/__tests__/src/test_calculateSpectrum.py
@@ -13,4 +13,45 @@ def test_calc_spectrum():
     assert "y" in data
     assert "units" in data
 
+# Slowest test
+def test_exomol_database():
+    modified_payload = payload_data.copy()
+    modified_payload.update({
+        "database": "exomol",
+    })
 
+    response = client.post("/calculate-spectrum", json=modified_payload)
+    
+    data = response.json()["data"]
+    assert response.status_code == 200
+    assert "x" in data
+    assert "y" in data
+    assert "units" in data
+
+def test_nist_database():
+    modified_payload = {
+        "min_wavenumber_range": 9679,
+        "max_wavenumber_range": 20074,
+        "species": [
+            {
+                "molecule": "N_II",
+                "mole_fraction": 0.1,
+            }
+        ],
+        "pressure": 20,
+        "tgas": 9000,
+        "path_length": 1.0,
+        "use_simulate_slit": "false",
+        "mode": "radiance_noslit",
+        "database": "nist",
+        "wavelength_units": "1/u.cm",
+        "pressure_units": "u.bar",
+        "path_length_units": "u.cm",
+    }
+    response = client.post("/calculate-spectrum", json=modified_payload)
+    
+    data = response.json()["data"]
+    assert response.status_code == 200
+    assert "x" in data
+    assert "y" in data
+    assert "units" in data
