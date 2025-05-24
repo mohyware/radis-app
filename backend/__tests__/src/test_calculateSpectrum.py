bebedb33e0d5aa367885d14c30aa1f351fb5c50d
Mohy Elden <109040862+mohyware@users.noreply.github.com>
2025-05-24 19:20:58 +0300

    Improve backend test coverage (#742)
    * test: enhance tests coverage in the backend
    
    * linting
    
    ---------
    
    Co-authored-by: Arunava Basu <arunavabasuDev@gmail.com>

--- backend/__tests__/src/test_calculateSpectrum.py
+++ backend/__tests__/src/test_calculateSpectrum.py
@@ -1,14 +1,92 @@
-from src.main import app   # assuming your FastAPI app instance is named 'app'
+""" testing calculateSpectrum.py """
+from unittest.mock import patch, MagicMock
 from fastapi.testclient import TestClient
+import numpy as np
+from radis.misc.warning import EmptyDatabaseError
+from src.main import app
 from __tests__.helpers.payload_data import payload_data
+
 client = TestClient(app)
 
-def test_calc_spectrum():
-    
-    response = client.post("/calculate-spectrum", json=payload_data)
-    
-    data = response.json()["data"]
+
+def test_calc_spectrum_with_simulate_slit_cm1():
+    """
+    testing /calculate-spectrum endpoint with simulate_slit and wavelength units in cm-1
+    """
+    modified_payload = payload_data.copy()
+    modified_payload.update({
+        "use_simulate_slit": True,
+        "wavelength_units": "1/u.cm"
+    })
+    response = client.post("/calculate-spectrum", json=modified_payload)
     assert response.status_code == 200
-    assert "x" in data
-    assert "y" in data
-    assert "units" in data
+    assert "data" in response.json()
+
+
+def test_calc_spectrum_with_simulate_slit_nm():
+    """
+    testing /calculate-spectrum endpoint with simulate_slit and wavelength units in nm
+    """
+    modified_payload = payload_data.copy()
+    modified_payload.update({
+        "use_simulate_slit": True,
+        "wavelength_units": "u.nm"
+    })
+    response = client.post("/calculate-spectrum", json=modified_payload)
+    assert response.status_code == 200
+    assert "data" in response.json()
+
+
+@patch("src.routes.calculateSpectrum.calculate_spectrum")
+def test_calc_spectrum_empty_database_error(mock_calc):
+    """
+    testing /calculate-spectrum endpoint when EmptyDatabaseError occurs
+    """
+    mock_calc.side_effect = EmptyDatabaseError("Empty DB")
+
+    response = client.post("/calculate-spectrum", json=payload_data)
+    assert response.status_code == 200
+    assert "error" in response.json()
+    assert response.json(
+    )["error"] == "No line in the specified wavenumber range"
+
+
+@patch("src.routes.calculateSpectrum.calculate_spectrum")
+def test_calc_spectrum_generic_exception(mock_calc):
+    """
+    testing /calculate-spectrum endpoint when generic exception occurs
+    """
+    mock_calc.side_effect = Exception("Unexpected error")
+
+    response = client.post("/calculate-spectrum", json=payload_data)
+    assert response.status_code == 200
+    assert "error" in response.json()
+    assert response.json()["error"] == "Unexpected error"
+
+
+@patch("src.routes.calculateSpectrum.calculate_spectrum")
+def test_calc_spectrum_large_payload(mock_calc):
+    """
+    testing /calculate-spectrum endpoint with large payload
+    """
+    payload_data["mode"] = "absorbance"
+    mock_spectrum = MagicMock()
+    mock_spectrum.__len__.return_value = int(1e7)
+    mock_spectrum.get_waveunit.return_value = "u.nm"
+    mock_spectrum.units = {"absorbance": "mW/cm2/sr/cm-1"}
+
+    x = np.linspace(0, 1000, 10000)
+    y = np.linspace(0, 1, 10000)
+
+    mock_spectrum.get.return_value = (x, y)
+
+    mock_calc.return_value = mock_spectrum
+
+    response = client.post("/calculate-spectrum", json=payload_data)
+    assert response.status_code == 200
+    result = response.json()["data"]
+    assert "x" in result and "y" in result and "units" in result
+    assert len(result["x"]) < len(x)
+
+    assert "x" in result and "y" in result and "units" in result
+    assert len(result["x"]) < 10000
--- backend/__tests__/src/test_downloadSpectrum.py
+++ backend/__tests__/src/test_downloadSpectrum.py
@@ -1,17 +1,51 @@
-from src.main import app
+""" testing downloadSpectrum.py """
+from unittest.mock import patch
 from fastapi.testclient import TestClient
+from radis.misc.warning import EmptyDatabaseError
+from src.main import app
 from __tests__.helpers.payload_data import payload_data
+
 client = TestClient(app)
+
 
 def test_download_spec():
     """
     testing /download-spectrum endpoint 
     """
     response = client.post("/download-spectrum", json=payload_data)
-    
+
     assert response.status_code == 200
     assert response.headers["content-type"] == "application/octet-stream"
     content_disposition = response.headers["content-disposition"]
-    filename_start = content_disposition.index("filename*=utf-8''") + len("filename*=utf-8''")
+    filename_start = content_disposition.index("filename*=utf-8''") + len(
+        "filename*=utf-8''")
     expected_extension = ".spec"  # Adjust with the expected file extension
     assert content_disposition[filename_start:].endswith(expected_extension)
+
+
+@patch("src.routes.downloadSpectrum.calculate_spectrum")
+def test_download_spec_empty_database_error(mock_calc):
+    """
+    testing /download-spectrum endpoint when EmptyDatabaseError occurs
+    """
+    mock_calc.side_effect = EmptyDatabaseError("Empty DB")
+
+    response = client.post("/download-spectrum", json=payload_data)
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "error": "No line in the specified wavenumber range"
+    }
+
+
+@patch("src.routes.downloadSpectrum.calculate_spectrum")
+def test_download_spec_generic_exception(mock_calc):
+    """
+    testing /download-spectrum endpoint when generic exception occurs
+    """
+    mock_calc.side_effect = Exception("Unexpected error")
+
+    response = client.post("/download-spectrum", json=payload_data)
+
+    assert response.status_code == 200
+    assert response.json() == {"error": "Unexpected error"}
